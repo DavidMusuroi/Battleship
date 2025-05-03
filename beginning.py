b9511ccc36554b9043fcc406b8493645c8b28274
David_M <titans156@gmail.com>
2025-05-03 22:39:38 +0300

    Added beginning.py

---
+++ beginning.py
@@ -0,0 +1,5 @@
+def beginning_game():
+    print("Welcome to Battleship!")
+    print("To play the game, either choose a classic setup or make your own custom one.")
+    print("The goal is for you to sink the enemy ships before your counterpart does.")
+    print("Good luck!", end = "\n\n")

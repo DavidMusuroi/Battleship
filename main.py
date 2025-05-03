85ace4df1c76ec15060b7b31f177ff4100f91497
David_M <titans156@gmail.com>
2025-05-04 01:31:41 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from init_grids import get_grids_length as len
+from init_grids import get_grids_length as length
 from init_grids import get_matrix as mat
 from beginning import beginning_game as start
+from place_ships_p1 import get_ship_placements_p1 as ships_p1
 
 start()
-m, n = len()
+m, n = length()
 p1_matrix = mat(m, n)
+p2_matrix = mat(m, n)
+dict_ships = {
+    "Destroyer" : 2,
+    "Submarine" : 3,
+    "Cruiser" : 4,
+    "Battleship" : 5
+}
+p1_hidden_matrix = ships_p1(p1_matrix)
+for i in range(m):
+    for j in range(n):
+        print(p1_hidden_matrix[i][j], end = " ")
+    print("\n")

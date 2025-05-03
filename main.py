9077e3ddf89385d771c1312b711aeaf7258baeb4
David_M <titans156@gmail.com>
2025-05-03 22:55:20 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from init_grids import get_grids_length as len
+from init_grids import get_matrix as mat
+from beginning import beginning_game as start
 
+start()
 m, n = len()
-print(m, n)
+p1_matrix = mat(m, n)

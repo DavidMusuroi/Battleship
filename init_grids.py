7450da213f685a170e1d0f00ea5a4d377f32f3ca
David_M <titans156@gmail.com>
2025-05-03 22:56:11 +0300

    Modified init_grids.py

--- init_grids.py
+++ init_grids.py
@@ -1,5 +1,4 @@
 def get_grids_length():
-
     print("Would you like a classic board or a custom board?")
     while 1:
         choice = input()
@@ -16,17 +15,25 @@ def get_grids_length():
                     if 6 <= m <= 10:
                         n = int(input())
                         if 6 <= n <= 10:
-                            print("Alright! Let's get the show on the board!")
                             okay = True
                         else:
                             print("Error: Incorrect value! Please choose two values between 6 and 10.")
                     else:
                         print("Error: Incorrect value! Please choose two values between 6 and 10.")
                 except ValueError:
-                    print("Error: No integer detected! Please try again.")
+                    print("Error: No number detected! Please try again.")
             if okay == True:
                 break
         else:
             print("Error: Unexpected text received! Please try again")
+    print("\nAlright! Let's get the show on the board!", end = "\n\n")
     return m, n
 
+def get_matrix(m = 6, n = 6):
+    matrix = []
+    for i in range(m):
+        tmp = []
+        for j in range(n):
+            tmp.append('-')
+        matrix.append(tmp)
+    return matrix

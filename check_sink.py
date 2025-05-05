f54fd640b3431996784981ef3bb971b00feaeb50
David_M <titans156@gmail.com>
2025-05-05 16:17:09 +0300

    Added check_sink.py

---
+++ check_sink.py
@@ -0,0 +1,9 @@
+def check_sink(matrix, char):
+    print(char)
+    m = len(matrix)
+    n = len(matrix[0])
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == char:
+                return False
+    return True

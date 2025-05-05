73e4c70ab9d386d882c9378437916c387f48b072
David_M <titans156@gmail.com>
2025-05-05 16:34:22 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ p1_hidden_matrix = ships_p1(p1_matrix)
 p1_matrix = mat(m, n)
 p2_hidden_matrix = ships_p2(p2_matrix)
 
-print("----Player 1 - Hidden ---\n")
+print("\n----Player 1 - Hidden ---\n")
 print_grid(p1_hidden_matrix)
 print("----Player 2 - Hidden ---\n")
 print_grid(p2_hidden_matrix)
@@ -25,11 +25,19 @@ print_grid(p2_hidden_matrix)
 while end == False:
     p2_matrix = player_go(p2_matrix, p2_hidden_matrix)
     if check_end(p2_matrix) == True:
+        print("Here was the original setup of player 1's ships:")
+        print_grid(p1_hidden_matrix)
+        print("You got this far:")
+        print_grid(p1_matrix)
         break
     else:
         print("The game continues. It's player 2's turn.")
     p1_matrix = player_go(p1_matrix, p1_hidden_matrix)
     if check_end(p1_matrix) == True:
+        print("Here was the original setup of player 2's ships:")
+        print_grid(p2_hidden_matrix)
+        print("You got this far:")
+        print_grid(p2_matrix)
         break
     else:
         print("The game continues. It's player 1's turn.")

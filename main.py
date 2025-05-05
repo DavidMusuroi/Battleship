e1221016fc3e7d0da8186f81278e6ee7d66f7887
Dulgheru Teodora <dulgheru.teodora26@gmail.com>
2025-05-05 18:51:25 +0300

    Modified main.py: print hidden boards to a file for reference pass a copy of p2_hidden_matrix with different reference to player_go

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from player_turn import player_go
 from check_end import check_end
 from print_grid import print_grid
 
+import sys
+import copy
+
 start()
 m, n = length()
 p1_matrix = mat(m, n)
@@ -14,30 +17,50 @@ p2_matrix = mat(m, n)
 end = False
 
 p1_hidden_matrix = ships_p1(p1_matrix)
+print(
+    "Player 1's ships have been placed on the board.\n\nNow it's Player 2's turn to place the ships.\n"
+)
+
+# print hidden boards to file for reference
+f = open("hidden_boards.out", "w")
+cpy = sys.stdout
+sys.stdout = f
+print("---Player 1's ships---\n")
+print_grid(p1_hidden_matrix)
 p1_matrix = mat(m, n)
+sys.stdout = cpy
+
 p2_hidden_matrix = ships_p2(p2_matrix)
 
-print("\n----Player 1 - Hidden ---\n")
-print_grid(p1_hidden_matrix)
-print("----Player 2 - Hidden ---\n")
+sys.stdout = f
+print("---Player 2's ships---\n")
 print_grid(p2_hidden_matrix)
+sys.stdout = cpy
 
+f.close()
+
+
+# lists will be modified in player_go
+p2_hidden_cpy = copy.deepcopy(p2_hidden_matrix)
+p1_hidden_cpy = copy.deepcopy(p1_hidden_matrix)
+
+print("It's player 1's turn.\n")
 while end == False:
-    p2_matrix = player_go(p2_matrix, p2_hidden_matrix)
+    p2_matrix = player_go(p2_matrix, p2_hidden_cpy)
     if check_end(p2_matrix) == True:
         print("Here was the original setup of player 1's ships:")
         print_grid(p1_hidden_matrix)
         print("You got this far:")
         print_grid(p1_matrix)
         break
-    else:
-        print("The game continues. It's player 2's turn.")
-    p1_matrix = player_go(p1_matrix, p1_hidden_matrix)
+
+    print("The game continues. It's player 2's turn.")
+    p1_matrix = player_go(p1_matrix, p1_hidden_cpy)
     if check_end(p1_matrix) == True:
         print("Here was the original setup of player 2's ships:")
         print_grid(p2_hidden_matrix)
         print("You got this far:")
         print_grid(p2_matrix)
         break
-    else:
-        print("The game continues. It's player 1's turn.")
+
+    print("The game continues. It's player 1's turn.")
